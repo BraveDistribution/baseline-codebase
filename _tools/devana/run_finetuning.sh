145e31c0665fe979af9de081dba3d051a4ddc1b8
khamyl <4025815+khamyl@users.noreply.github.com>
2025-08-23 15:26:03 +0200

    Devana finetune

---
+++ _tools/devana/run_finetuning.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Ask user for task ID
+read -p "Enter task ID (1, 2, or 3): " TASKID
+
+# Validate input
+if [[ ! "$TASKID" =~ ^[1-3]$ ]]; then
+    echo "Error: Invalid task ID. Please enter 1, 2, or 3."
+    exit 1
+fi
+
+LOG_FILE="$SCRIPT_DIR/run_finetuning_task${TASKID}.log"
+
+#exec > "$LOG_FILE" 2>&1
+
+echo cuda_visible_devices: $CUDA_VISIBLE_DEVICES
+
+# Activate environment
+source /home/mg873uh/Projects_kb/.venv_fomo/bin/activate
+
+# Change to project directory
+cd /home/mg873uh/Projects_kb/baseline-codebase
+
+python src/finetune.py \
+    --data_dir=/projects/p1170-25-1/data/finetuning_preproc \
+    --save_dir=/home/mg873uh/Fomo25/baseline-codebase/_models/finetune \
+    --pretrained_weights_path=/home/mg873uh/Fomo25/baseline-codebase/_models/models/FOMO60k_2.667mm_float16/unet_b_lw_dec/versions/version_0/last.ckpt \
+    --model_name=unet_b \
+    --patch_size=96 \
+    --taskid=${TASKID} \
+    --batch_size=4 \
+    --epochs=500 \
+    --train_batches_per_epoch=100 \
+    --augmentation_preset=basic
