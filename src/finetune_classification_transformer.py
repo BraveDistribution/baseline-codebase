f18f15efaaffbd6fd50a1eac4e493105917a4491
khamyl <kamil.barbieirk@gmail.com>
2025-08-27 15:48:46 +0200

    mato

--- src/finetune_classification_transformer.py
+++ src/finetune_classification_transformer.py
@@ -13,7 +13,7 @@ from yucca.modules.data.augmentation.YuccaAugmentationComposer import (
     YuccaAugmentationComposer,
 )
 from yucca.pipeline.configuration.split_data import get_split_config
-from mato_models.models import ClassificationFineTuner, RegressionFineTuner, SegmentationFineTuner, ClassificationFinetuner2, RegressionFinetuner2, RegressionFinetuner3
+from mato_models.models import ClassificationFineTuner, RegressionFineTuner, SegmentationFineTuner, ClassificationFinetuner2, RegressionFinetuner2, RegressionFinetuner3, RegressionFinetuner4, SegmentationProtoNet
 from yucca.modules.data.data_modules.YuccaDataModule import YuccaDataModule
 from yucca.modules.callbacks.loggers import YuccaLogger
 from yucca.modules.data.datasets.YuccaDataset import YuccaTrainDataset
@@ -414,9 +414,10 @@ def train(
         monitor=monitor,
         mode="min",
         save_top_k=5,
+        save_last=True,
     )
     # aug_params = get_finetune_augmentation_params("all")
-    aug_params = get_finetune_augmentation_params("all")
+    aug_params = get_finetune_augmentation_params("basic")
     # aug_params["crop"] = True
     # aug_params["random_crop"] = False
     task_type_preset = "classification" if task_type == "regression" else task_type
@@ -487,14 +488,14 @@ def train(
             max_epochs=50
         )
     elif task_type == "regression":
-        model = RegressionFinetuner2.load_from_checkpoint(
-            str(model_checkpoint),
-            in_channels=num_modalities,
-            freeze_encoder=True,
-            learning_rate=1e-4,
-            max_epochs=50,
-            strict=False
-        )
+        # model = RegressionFinetuner2.load_from_checkpoint(
+        #     str(model_checkpoint),
+        #     in_channels=num_modalities,
+        #     freeze_encoder=True,
+        #     learning_rate=1e-4,
+        #     max_epochs=50,
+        #     strict=False
+        # )
         model = RegressionFinetuner3.load_from_pretrained(
             checkpoint_path=str(model_checkpoint),
             in_channels=2,
@@ -506,7 +507,7 @@ def train(
         )
 
     elif task_type == 'segmentation':
-        model = SegmentationFineTuner.load_from_pretrained(
+        model = SegmentationProtoNet.load_from_pretrained(
             str(model_checkpoint),
             num_classes=2,
             in_channels=3,
